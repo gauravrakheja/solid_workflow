e894287e56cee393b7a56c9489270a94100fbe22
gaurav-rakheja <gaurav.rakheja@go-jek.com>
2020-02-05 18:11:17 +0530

    creates initial repositoy

---
+++ lib/solid_workflow.rb
@@ -0,0 +1,6 @@
+require "solid_workflow/version"
+
+module SolidWorkflow
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/solid_workflow/version.rb
@@ -0,0 +1,3 @@
+module SolidWorkflow
+  VERSION = "0.1.0"
+end
